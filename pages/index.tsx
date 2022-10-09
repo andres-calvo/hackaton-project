04444f64d24078378a7c3b8aedda714e403126b7
Andres Calvo Ariza <andres.calvoa@udea.edu.co>
2022-10-09 08:29:52 -0500

    Add Loading and Swap Functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,43 @@
 import type { NextPage } from "next";
-import { RiArrowDropDownLine } from "react-icons/ri";
-import { ConnectButton } from "@web3modal/react";
-import { useAccount, useNetwork } from "@web3modal/react";
-import { useRead } from "../src/store";
+import { ConnectButton, useWaitForTransaction } from "@web3modal/react";
+import { useAccount } from "@web3modal/react";
+import { useWriteTx } from "../src/store";
+import { useForm } from "react-hook-form";
+import { Loading } from "../src/loading";
+import { useEffect } from "react";
+import Swal from "sweetalert2";
 
 const Home: NextPage = () => {
   const { address, isConnected } = useAccount();
-  const { chain, chains } = useNetwork();
-  const { data } = useRead();
-  console.log(`
-  
-  Address ${address}
-  isConnected ${isConnected}
-  chain ${chain?.network}
+  const { write, data: swapData, isLoading } = useWriteTx();
+  const { register, getValues, formState, setValue } = useForm({
+    mode: "onChange",
+    defaultValues: {
+      token1: 0,
+      token2: 0,
+    },
+  });
+  const {
+    receipt: transactionData,
+    error,
+    isWaiting: isLoadingTransaction,
+  } = useWaitForTransaction({
+    hash: swapData?.hash,
+  });
+  const { isValid } = formState;
 
-  data contract ${data}
-
-  `);
+  useEffect(() => {
+    if (transactionData && !isLoadingTransaction) {
+      const { token1, token2 } = getValues();
+      Swal.fire(
+        "Success",
+        `You have exchanged ${token1}RCOP for ${token2}RARG`
+      );
+    }
+  }, [transactionData, swapData]);
+  const onSwapClick = () => {
+    write();
+  };
   return (
     <section className="h-screen w-screen">
       <div className="flex justify-center w-full items-center h-full">
@@ -26,16 +47,58 @@ const Home: NextPage = () => {
         >
           <span className="p-4 text-slate-300 font-bold">Swap</span>
           <div className="flex justify-between p-4">
-            <InputAmount />
+            <input
+              inputMode="decimal"
+              title="Token Amount"
+              autoComplete="off"
+              autoCorrect="off"
+              spellCheck="false"
+              type="text"
+              pattern="^[0-9]*[.,]?[0-9]*$"
+              placeholder="0"
+              min="0"
+              minLength={1}
+              maxLength={79}
+              className="text-left text-base md:text-sm placeholder:font-normal font-medium p-0 bg-transparent border-none focus:outline-none focus:ring-0 w-full truncate font-medium !text-3xl py-1 text-slate-200 hover:text-slate-100"
+              {...register("token1", {
+                required: true,
+                onChange: (e) => {
+                  setValue("token2", e.target.value/30.9);
+                },
+              })}
+            />
             <ButtonTokens token="RCOP" />
           </div>
           <div className="flex flex-col bg-slate-800 p-4">
             <div className="flex justify-between  ">
-              <InputAmount />
+              <input
+                inputMode="decimal"
+                title="Token Amount"
+                autoComplete="off"
+                autoCorrect="off"
+                spellCheck="false"
+                type="text"
+                pattern="^[0-9]*[.,]?[0-9]*$"
+                placeholder="0"
+                min="0"
+                disabled={true}
+                minLength={1}
+                maxLength={79}
+                className="text-left text-base md:text-sm placeholder:font-normal font-medium p-0 bg-transparent border-none focus:outline-none focus:ring-0 w-full truncate font-medium !text-3xl py-1 text-slate-200 hover:text-slate-100"
+                {...register("token2", {})}
+              />
               <ButtonTokens token="RARG" />
             </div>
             <div className="w-full flex justify-center mt-10">
-              {isConnected ? <Swap /> : <ConnectButton />}
+              {isConnected ? (
+                <Swap
+                  disabled={!isValid}
+                  onClick={onSwapClick}
+                  isLoading={isLoading || isLoadingTransaction}
+                />
+              ) : (
+                <ConnectButton />
+              )}
             </div>
           </div>
         </div>
@@ -51,15 +114,12 @@ const ButtonTokens = ({ token = "" }) => {
     <button className="bg-slate-600 shadow-2xl rounded-xl px-2 py-1 ">
       <div className="flex items-center text-white">
         <span className="">{token}</span>
-        {/* <div className="text-3xl">
-          <RiArrowDropDownLine className="text-white" />
-        </div> */}
       </div>
     </button>
   );
 };
 
-const Swap = ({ onClick = () => {} }) => {
+const Swap = ({ onClick = () => {}, disabled = false, isLoading = false }) => {
   return (
     <button
       className="
@@ -71,27 +131,9 @@ const Swap = ({ onClick = () => {} }) => {
           mt-4
           "
       onClick={onClick}
+      disabled={disabled}
     >
-      Swap
+      {!isLoading ? "Swap" : <Loading />}
     </button>
-  );
-};
-
-const InputAmount = () => {
-  return (
-    <input
-      inputMode="decimal"
-      title="Token Amount"
-      autoComplete="off"
-      autoCorrect="off"
-      spellCheck="false"
-      type="text"
-      pattern="^[0-9]*[.,]?[0-9]*$"
-      placeholder="0"
-      min="0"
-      minLength={1}
-      maxLength={79}
-      className="text-left text-base md:text-sm placeholder:font-normal font-medium p-0 bg-transparent border-none focus:outline-none focus:ring-0 w-full truncate font-medium !text-3xl py-1 text-slate-200 hover:text-slate-100"
-    />
   );
 };
--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,6 @@
 import testABI from "./testABI.json";
-import { useContractRead } from "@web3modal/react";
-import { useAccount, useNetwork } from "@web3modal/react";
+import { useContractRead, useContractWrite } from "@web3modal/react";
+import { useNetwork } from "@web3modal/react";
 
 const GoerliSmartContract = "0xDAA5D0365071bD6b55C66f5A2900b0bb3A8B132F";
 const MumbaiSmartContract = "0x873FA9AC25fF215ed14aCD4C88823c2aD9c6663a";
@@ -16,4 +16,14 @@ export function useRead() {
   });
 }
 
-export function useWriteMessage() {}
+export function useWriteTx() {
+  const { chain } = useNetwork();
+  const contractAddress =
+    chain?.network == "goerli" ? GoerliSmartContract : MumbaiSmartContract;
+  return useContractWrite({
+    addressOrName: contractAddress,
+    contractInterface: testABI,
+    functionName: "setMessage",
+    args: [chain?.network == "goerli" ? "GoerliMessage" : "MumbaiMessage"],
+  });
+}
